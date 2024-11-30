3bb18b4624279985c60fa4bc7c7f9b82f63ff1a4
avbhutani <avbhutani3@gmail.com>
2024-11-30 22:19:20 +0530

    Added the middleware to allow only logged in users to access the backend api's

--- server/routes/followRoutes/followRoute.js
+++ server/routes/followRoutes/followRoute.js
@@ -1,10 +1,33 @@
 const followersController = require('../../controllers/followersControllers/followersController')
 const express = require('express')
+const checkAuthToken = require('../../utility/checkAuthToken')
 const followingController = require('../../controllers/followingControllers/followingController')
 const followRoute = express()
 
 
-followRoute.get('/api/fetch/followers/:username',followersController)
-followRoute.get('/api/fetch/following/:username',followingController)
+followRoute.get('/api/fetch/followers/:username',(req,res,next) => {
+    const token = req.headers.authtoken 
+    if(!token) {
+        res.status(400).send(
+            {
+                success:'false',
+                message:'Kindly Login!'
+            }
+        )
+    }
+    next()
+},followersController)
+followRoute.get('/api/fetch/following/:username',(req,res,next) => {
+    const token = req.headers.authtoken 
+    if(!token) {
+        res.status(400).send(
+            {
+                success:'false',
+                message:'Kindly Login!'
+            }
+        )
+    }
+    next()
+},followingController)
 
 module.exports = followRoute
